4edda8f10e52ba38e256cbd7d64cd9964e3843ff
Edmarck Sosa Pineda <e.sosa@csu.fullerton.edu>
2024-09-21 21:33:46 -0700

    Cleaned up code a bit, modified add_contact

--- contacts.py
+++ contacts.py
@@ -1,22 +1,19 @@
 # Edmarck Sosa, Due by 9/10/24, a Python program that performs as a Tuffy Titan Contact List which contains a list of contacts that can be modified or deleted.
 
-def print_list(contacts):
-    """Prints the list of names. Args: contact_list (_type_): _description_."""
-    print(f"{'Index':8}{'First Name':22}{'Last Name':22}")
-    for i in range(len(contacts)):
-        print(f'{str(i):8}{contacts[i][0]:22}{contacts[i][1]:22}')
+# def print_list(contacts):
+#     """Prints the list of names. Args: contact_list (_type_): _description_."""
+#     print(f"{'Index':8}{'First Name':22}{'Last Name':22}")
+#     for i in range(len(contacts)):
+#         print(f'{str(i):8}{contacts[i][0]:22}{contacts[i][1]:22}')
+# since the readme did not say to add the print list on contacts.py it will be in main
 
-def add_contact(contacts, first_name, last_name):
+def add_contact(contact, first_name, last_name, id = None):
     """Adds contact to the list of names.  Args: add_contact (contacts): _description_."""
-    # first_name = firstName
-    # last_name = lastName
-    # lastName = last_name
-    # firstName = first_name
-    #cfirstName = input("Enter first name: ")
-    # lastName = input("Enter last name: ")
-    #contacts.append([[firstName], [lastName]])
-    contacts.append([first_name, last_name])
-    return contacts
+    contact = {}
+    if id in contact:
+        print("error")
+    contact[id] = [first_name, last_name]
+    return {id: contact[id]}
     
 def modify_contact(contacts, first_name, last_name, index):
     """Modifies the contact list. Args: modify_contact(contact_list) _description_."""
@@ -62,19 +59,6 @@ def sort_contacts(contacts, column):
             # contacts.sort(key=lambda x: x[1])
             contacts.sort(key = lambda x: x[1])
 
-            #return
-        
-        #sorted(contacts, key = lambda contacts: contacts[1] )
+def find_contact(contacts, find):
+    name = {}
     
-    # return contacts
-
-# def multiple(n):
-#     return lambda x: X + n
-# multiple_2 = multiple(2) prints 20
-# multiple_3 = multiple(3) prints 30
-
-# def practice(self):
-#     students = [["james", 34], ["adam", 21], ["Kyle", 18]]
-#     sorted_students = sorted(students, key = lambda x: x[1])
-#     students.sort(key=lambda x: x[1])
-#     print(sorted_students)    
